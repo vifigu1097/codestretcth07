0177b7547ae7c868abfc3176fa39d172616668e4
Linaa Finklea <sky@linaas-mbp.mynetworksettings.com>
2023-10-24 10:19:25 -0400

    codestretch

---
+++ index.js
@@ -0,0 +1,11 @@
+// create and set variables
+// const accountBalance = 200
+// hasPaid = false
+// message ='Your balance is [${accountBalance}]'
+// make an alert with message
+//set haspaid to true
+// alert the 
+
+const accountBalance = 200;
+console.log(accountBalance);
+
